696c8a40be1801c5fc333c0f8f6081ef1969dabb
Lillian Brevik <77015337+lillian-se@users.noreply.github.com>
2021-09-30 16:15:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+let nr1 = document.getElementById("nr1");
+let nr2 = document.getElementById("nr2");
+let nr3 = document.getElementById("nr3");
+let nr4 = document.getElementById("nr4");
+let nr5 = document.getElementById("nr5");
+if (new Date() >= new Date("Thu Sep 30 2021 0:01")) {
+  nr1.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>1</h3></a>";
+  nr2.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>2</h3></a>";
+  nr3.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>3</h3></a>";
+  nr4.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>4</h3></a>";
+  nr5.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>5</h3></a>";
+} else {
+  nr1.innerHTML = "<a href='senare.html'><h3>1</h3></a>";
+  nr2.innerHTML = "<a href='senare.html'><h3>2</h3></a>";
+  nr3.innerHTML = "<a href='senare.html'><h3>3</h3></a>";
+  nr4.innerHTML = "<a href='senare.html'><h3>4</h3></a>";
+  nr5.innerHTML = "<a href='senare.html'><h3>5</h3></a>";
+}
+//====================================v2=====================================
+
+let nr6 = document.getElementById("nr6");
+let nr7 = document.getElementById("nr7");
+let nr8 = document.getElementById("nr8");
+let nr9 = document.getElementById("nr9");
+let nr10 = document.getElementById("nr10");
+let nr11 = document.getElementById("nr11");
+let nr12 = document.getElementById("nr12");
+if (new Date() >= new Date("Mon Dec 6 2021 0:01")) {
+  nr6.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>6</h3></a>";
+  nr7.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>7</h3></a>";
+  nr8.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>8</h3></a>";
+  nr9.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>9</h3></a>";
+  nr10.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>10</h3></a>";
+  nr11.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>11</h3></a>";
+  nr12.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>12</h3></a>";
+} else {
+  nr6.innerHTML = "<a href='senare.html'><h3>6</h3></a>";
+  nr7.innerHTML = "<a href='senare.html'><h3>7</h3></a>";
+  nr8.innerHTML = "<a href='senare.html'><h3>8</h3></a>";
+  nr9.innerHTML = "<a href='senare.html'><h3>9</h3></a>";
+  nr10.innerHTML = "<a href='senare.html'><h3>10</h3></a>";
+  nr11.innerHTML = "<a href='senare.html'><h3>11</h3></a>";
+  nr12.innerHTML = "<a href='senare.html'><h3>12</h3></a>";
+}
+
+//======================================V3=====================================
+let nr13 = document.getElementById("nr13");
+let nr14 = document.getElementById("nr14");
+let nr15 = document.getElementById("nr15");
+let nr16 = document.getElementById("nr16");
+let nr17 = document.getElementById("nr17");
+let nr18 = document.getElementById("nr18");
+let nr19 = document.getElementById("nr19");
+if (new Date() >= new Date("Mon Dec 13 2021 0:01")) {
+  nr13.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>13</h3></a>";
+  nr14.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>14</h3></a>";
+  nr15.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>15</h3></a>";
+  nr16.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>16</h3></a>";
+  nr17.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>17</h3></a>";
+  nr18.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>18</h3></a>";
+  nr19.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>19</h3></a>";
+} else {
+  nr13.innerHTML = "<a href='senare.html'><h3>13</h3></a>";
+  nr14.innerHTML = "<a href='senare.html'><h3>14</h3></a>";
+  nr15.innerHTML = "<a href='senare.html'><h3>15</h3></a>";
+  nr16.innerHTML = "<a href='senare.html'><h3>16</h3></a>";
+  nr17.innerHTML = "<a href='senare.html'><h3>17</h3></a>";
+  nr18.innerHTML = "<a href='senare.html'><h3>18</h3></a>";
+  nr19.innerHTML = "<a href='senare.html'><h3>19</h3></a>";
+}
+let nr20 = document.getElementById("nr20");
+let nr21 = document.getElementById("nr21");
+let nr22 = document.getElementById("nr22");
+let nr23 = document.getElementById("nr23");
+let nr24 = document.getElementById("nr24");
+if (new Date() >= new Date("Mon Dec 20 2021 0:01")) {
+  nr20.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>20</h3></a>";
+  nr21.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>21</h3></a>";
+  nr22.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>22</h3></a>";
+  nr23.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>23</h3></a>";
+  nr24.innerHTML =
+    "<a href='https://www.google.com/' target='blank'><h3>24</h3></a>";
+} else {
+  nr20.innerHTML = "<a href='senare.html'><h3>20</h3></a>";
+  nr21.innerHTML = "<a href='senare.html'><h3>21</h3></a>";
+  nr22.innerHTML = "<a href='senare.html'><h3>22</h3></a>";
+  nr23.innerHTML = "<a href='senare.html'><h3>23</h3></a>";
+  nr24.innerHTML = "<a href='senare.html'><h3>24</h3></a>";
+}
